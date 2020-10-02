8c76786ed9ad20cb1693151354563b525bc37840
kazemicode <sarieses@gmail.com>
2020-10-02 02:09:03 -0700

    Added movie grid view

---
+++ flixter-ios/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixter-ios
+//
+//  Created by Sara Kazemi on 10/1/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
